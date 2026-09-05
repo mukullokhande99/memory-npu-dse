9501bbb74a7aa9cfcf68349f4d9998eaa6af1213
Mukul <36757243+mukullokhande99@users.noreply.github.com>
2026-09-05 22:14:58 +0530

    Initial memory-centric NPU DSE framework

---
+++ memory_npu/__init__.py
@@ -0,0 +1,5 @@
+"""First-order memory-centric NPU design-space exploration primitives."""
+
+from .model import MemoryTechnology, NPUConfig, Workload, evaluate
+
+__all__ = ["MemoryTechnology", "NPUConfig", "Workload", "evaluate"]
